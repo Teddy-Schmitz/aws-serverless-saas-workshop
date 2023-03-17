a778139a3104fbd55c53fd4919e120f377c69238
Teddy Schmitz <tedsch@stripe.com>
2023-03-17 15:51:57 +0000

    remove labs 4-7

--- Lab4/client/Application/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  regApiGatewayUrl:
-    'https://k1lecgl9ye.execute-api.us-west-2.amazonaws.com/prod/',
-  apiGatewayUrl: 'https://885cs6u6m8.execute-api.us-west-2.amazonaws.com/prod/',
-  userPoolId: 'us-west-2_bCwYPJqrb',
-  appClientId: '6lv6qhvmh6ivgd94qsftruc994',
-};
--- Lab5/client/Application/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  regApiGatewayUrl:
-    'https://ulp15c9bv2.execute-api.us-west-2.amazonaws.com/prod/',
-};
